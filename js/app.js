5f8c575e659b42b765b663fa2205554eacb325bd
Kevin Zheng <Kevin.Zheng4783@gmail.com>
2019-01-23 17:17:59 -0500

    Main content fixed and edited features

--- js/app.js
+++ js/app.js
@@ -3,11 +3,22 @@ const fade = document.querySelectorAll('.fade');
 
 window.addEventListener('scroll', function (e) {
     last_known_scroll_position = window.scrollY;
-    if (window.scrollY > 250) {
-        for (let i = 0; i < fade.length; i++) {
-            fade[i].classList.remove('fade');
-            fade[i].classList.add('fade-in');
+    if (window.scrollY > 400) {
+            fade[0].classList.remove('fade');
+            fade[0].classList.add('fade-in');
+        if (window.scrollY > 1000) {
+            fade[1].classList.remove('fade');
+            fade[1].classList.add('fade-in');
+            if (window.scrollY > 1600) {
+                fade[2].classList.remove('fade');
+                fade[2].classList.add('fade-in');
+            }
         }
+        // All FADE
+        // for (let i = 0; i < fade.length; i++) {
+        //     fade[i].classList.remove('fade');
+        //     fade[i].classList.add('fade-in');
+        // }
     } else {
         for (let i = 0; i < fade.length; i++) {
             fade[i].classList.add('fade');
